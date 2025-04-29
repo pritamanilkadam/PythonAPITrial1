455d1ea17730aadb49752567788b709fb399391f
pritamanilkadam <pritamanilkadam@gmail.com>
2025-04-29 20:55:47 +0530

    Update Basic FastAPI.py
    Something hot fixed at main

--- Basic FastAPI.py
+++ Basic FastAPI.py
@@ -2,6 +2,6 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-@app.get("/")
+@app.get("/Message1")
 def read_root():
     return {"message": "I have made changes to code. My main branch has been updated."}
