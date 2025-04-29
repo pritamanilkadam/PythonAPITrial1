3ee5276754bf769d893197a48481aa94f31e2cff
pritamanilkadam <pritamanilkadam@gmail.com>
2025-04-29 22:50:32 +0530

    Update Basic FastAPI.py

--- Basic FastAPI.py
+++ Basic FastAPI.py
@@ -4,4 +4,4 @@ app = FastAPI()
 
 @app.get("/Message1")
 def read_root():
-    return {"message": "I have made changes to code. My main branch has been updated."}
+    return {"message": "I have made changes to code. My main branch has been updated. Something more"}
