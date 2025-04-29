77b2ecd1dca6d0096ebc65e290a02caec72df629
Pritam Kadam <PAKadam@burnsmcd.com>
2025-04-29 21:04:44 +0530

    Let add another code

--- Basic FastAPI.py
+++ Basic FastAPI.py
@@ -5,3 +5,8 @@ app = FastAPI()
 @app.get("/")
 def read_root():
     return {"message": "I have made changes to code"}
+
+
+@app.get("/Message2")
+def read_root():
+    return {"message": "This is message 2."}
